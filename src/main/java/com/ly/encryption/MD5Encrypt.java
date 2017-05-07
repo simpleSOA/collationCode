60ec411d0828100bc8f1367d8a8e39214a995b1d
linyang <123456>
2017-05-07 09:32:26 +0800

    重构md5

--- src/main/java/com/ly/encryption/MD5Encrypt.java
+++ src/main/java/com/ly/encryption/MD5Encrypt.java
@@ -3,50 +3,42 @@ package com.ly.encryption;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * @author: Administrator
  * Date: 2016/3/1 Time: 14:15
  */
 public class MD5Encrypt {
-
     private static final Charset ENCODING = StandardCharsets.UTF_8;
     //十六进制下数字到字符的映射数组
-    private final static String[] hexDigits = {"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"};
+    private final static char [] hexDigits = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+    private static final String MD5DIGEST = "MD5";
 
     /**
-     * MD5摘要
-     * @param originString 原文
+     * md5散列函数
+     * @param src 需要散列的字符串
+     * @return 大写后的散列值
      */
-    public static String encodeByMD5(String originString) throws Exception {
-        MessageDigest md = MessageDigest.getInstance("MD5");
-        byte[] results = md.digest(originString.getBytes(ENCODING));
-        //将得到的字节数组变成字符串返回
-        String result = byteArrayToHexString(results);
-        return result;
-    }
-
-    /**
-     * 轮换字节数组为十六进制字符串
-     * @param b 字节数组
-     * @return 十六进制字符串
-     */
-    private static String byteArrayToHexString(byte[] b){
-        StringBuffer resultSb = new StringBuffer();
-        for(int i=0;i<b.length;i++){
-            resultSb.append(byteToHexString(b[i]));
+    public final static String md5(String src) {
+        byte[] btInput = src.getBytes(ENCODING);
+        MessageDigest mdInst = null;
+        try {
+            mdInst = MessageDigest.getInstance(MD5DIGEST);
+        } catch (NoSuchAlgorithmException e) {
+            //java平台支持md5，忽略它
         }
-        return resultSb.toString();
-    }
-
-    //将一个字节转化成十六进制形式的字符串
-    private static String byteToHexString(byte b){
-        int n = b;
-        if(n<0){
-            n=256+n;
+        mdInst.update(btInput);
+        byte[] md = mdInst.digest();
+        // 把密文转换成十六进制的字符串形式
+        int j = md.length;
+        char str[] = new char[j * 2];
+        int k = 0;
+        for (int i = 0; i < j; i++) {
+            byte byte0 = md[i];
+            str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+            str[k++] = hexDigits[byte0 & 0xf];
         }
-        int d1 = n/16;
-        int d2 = n%16;
-        return hexDigits[d1] + hexDigits[d2];
+        return new String(str);
     }
 }
